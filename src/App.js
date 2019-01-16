b055e9b1fb4e9bee72480b5cef2be38417ce5443
jsiilva1 <jsiilva@outlook.com.br>
2019-01-16 16:35:03 -0200

    criando a chamada para a API via formulário.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import axios from 'axios'
 
 import Titles from './components/Titles'
 import Weather from './components/Weather'
+import Form from './components/Form'
 
 import apiKeys from './config//apiKeys'
 
 export default class App extends React.Component {
     
-    getWeather = async () => {
+    getWeather = async e => {
+        e.preventDefault();
+
+        const city = e.target.elements.city.value
+        const country = e.target.elements.country.value
+
         const callToApi = await 
-            axios(`https://samples.openweathermap.org/data/2.5/weather?q=Picos,BR&appid=${apiKeys.pubKey}`)
+            fetch(
+                `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKeys.pubKey}
+            `)
         
         const data = await callToApi.json()
         console.log(data)
@@ -21,6 +29,7 @@ export default class App extends React.Component {
         return (
             <div>
                 <Titles />
+                <Form getWeather={this.getWeather} />
                 <Weather />
             </div>
         )
--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,10 @@ import React from 'react'
 export default class Form extends React.Component {
     render () {
         return (
-            <form>
+            <form onSubmit={this.props.getWeather}>
                 <input type="text" name="city" placeholder="Cidade"/>
                 <input type="text" name="country" placeholder="País"/>
+                <button>Obter dados climáticos</button>
             </form>
         )   
     }
